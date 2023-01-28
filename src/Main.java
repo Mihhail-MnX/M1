6819f15cd523bd667f9f47da8c6ba289c884a154
Mihail Sharagov <seamike26@gmail.com>
2023-01-28 13:45:44 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cat barsik = new Cat(); // создали кота из класса Cat
+        Cat bigCat = new Cat(5500, "Murka", 2, "Black");
+        Cat smallCat = new Cat(1000, "Kuzya", 1, "White");
+        System.out.println(bigCat);
+        System.out.println(smallCat);
+    }
+}
